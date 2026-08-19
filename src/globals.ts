9f6b1ad57e903f205ece263fceee74bb806decb0
Steven Erik Martijn Roest <semroest@proton.me>
2026-08-19 11:37:11 +0200

    Add 404 and blog page, fix buttons and add globals file

---
+++ src/globals.ts
@@ -0,0 +1,6 @@
+export const menuItems = [
+    { label: "About", href: "/#about" },
+    { label: "Projects", href: "/#projects" },
+    { label: "Blog", href: "/blog" },
+    { label: "Contact", href: "/#contact" },
+];
